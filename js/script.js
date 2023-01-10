8477c72c25e3035c52092bd07a4f168550abe44c
WilliamLam94 <william.tup.lam@gmail.com>
2023-01-10 22:51:11 +0100

    update

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ for (let i = 0; i < 5; i++) {
 }
 const div = document.createElement('div');
 console.log(div);
+
+const arrOne = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (const list of arrOne) {
+	console.log(list);
+	document.body.append(list);
+}
+
+//const arrTwo = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+//for (const list of arrOne) {
+//	console.log(list);
+//	document.body.append(list);
+//}
