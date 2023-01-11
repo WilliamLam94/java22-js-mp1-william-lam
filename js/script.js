5d44e1c7e188eaad387b613cd11e523e0a9f5c01
WilliamLam94 <william.tup.lam@gmail.com>
2023-01-11 18:50:22 +0100

    update

--- js/script.js
+++ js/script.js
@@ -6,29 +6,50 @@ for (let i = 0; i < 5; i++) {
 	header.style.fontSize = `${16 + i * 2}px`;
 	header.style.textAlign = 'center';
 }
+
+//creates a div
 const div = document.createElement('div');
 document.body.append(div);
-console.log(div);
+div.style.border = '10px solid';
+div.style.padding = '10px';
+div.style.display = 'flex';
+div.style.justifyContent = 'space-around';
 
-const ol = document.createElement('ol');
-document.body.append(ol);
+const numbersArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for (let i = 0; i < 10; i++) {
-	const li = document.createElement('li');
-	ol.append(li);
-	li.innerText = `${i + 0}`;
-	li.style.listStyleType = 'none';
-}
+//0-9
+const listEl = document.createElement('ul');
+div.append(listEl);
+listEl.style.listStyleType = 'none';
+/** 
+ * TODO
+//* set background to purple
+//*make listitem black and white(nthchild)
+//*make innerText black and white
+//* make if statment for number 4,8 and sex
+**/
+numbersArr.map((number) => {
+	const listItemEl = document.createElement('li');
+	listItemEl.innerText = number;
+	listEl.append(listItemEl);
+});
 
-const ul = document.createElement('ul');
-document.body.append(ul);
+//9-0
 
-for (let i = 0; i < 10; i++) {
-	const li = document.createElement('li');
-	ul.append(li);
-	li.innerText = `${9 + i * 0}`;
-	li.style.listStyleType = 'none';
-}
+const reverseListEl = document.createElement('ul');
+div.append(reverseListEl);
+reverseListEl.style.listStyleType = 'none';
+
+const highToLowNumbersArr = numbersArr.sort((a, b) => b - a);
+console.log(highToLowNumbersArr);
+
+highToLowNumbersArr.map((number) => {
+	const reverseListItemEl = document.createElement('li');
+	reverseListItemEl.innerText = number;
+	reverseListEl.append(reverseListItemEl);
+});
+
+// ett-tio
 
 const arr = [
 	'ett ',
@@ -42,13 +63,14 @@ const arr = [
 	' nio ',
 	' tio ',
 ];
-for (const list of arr) {
-	console.log(list);
-	document.body.append(list);
-}
 
-//const arrTwo = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-//for (const list of arrOne) {
-//	console.log(list);
-//	document.body.append(list);
-//}
+const numbersInWordsEl = document.createElement('ul');
+div.append(numbersInWordsEl);
+numbersInWordsEl.style.listStyleType = 'none';
+
+for (const number of arr) {
+	const listItemEl = document.createElement('li');
+	listItemEl.innerText = number;
+	numbersInWordsEl.append(listItemEl);
+	console.log(number);
+}
