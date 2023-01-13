f4bcc18e4ac388f0c850240ded6830a9cb70cdce
WilliamLam94 <william.tup.lam@gmail.com>
2023-01-13 17:40:36 +0100

    last commit

--- js/script.js
+++ js/script.js
@@ -18,21 +18,14 @@ div.style.justifyContent = 'space-around';
 
 const numbersArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-/** 
- * TODO
-//* set background to purple
-//*make listitem black and white(nthchild)
-//*make innerText black and white(nthchild)
-//* make if statment for number 4,8 and sex
-**/
-
 //0-9
 const listEl = document.createElement('ul');
 div.append(listEl);
 listEl.style.listStyleType = 'none';
 listEl.style.textAlign = 'start';
 listEl.style.backgroundColor = '#a8a8f0';
-listEl.style.padding = '20px';
+listEl.style.padding = '10px';
+listEl.style.height = '100%';
 
 numbersArr.map((number) => {
 	const listItemEl = document.createElement('li');
@@ -40,13 +33,16 @@ numbersArr.map((number) => {
 	listEl.append(listItemEl);
 	listItemEl.style.backgroundColor = 'black';
 	listItemEl.style.color = 'white';
+	listItemEl.style.width = '50px';
 
 	if (number % 2 == 1) {
 		listItemEl.style.backgroundColor = 'white';
 		listItemEl.style.color = 'black';
 	}
-	number++;
-	console.log(number);
+
+	if (number === 4) {
+		listItemEl.style.backgroundColor = 'transparent';
+	}
 });
 
 //9-0
@@ -56,7 +52,8 @@ div.append(reverseListEl);
 reverseListEl.style.listStyleType = 'none';
 reverseListEl.style.textAlign = 'center';
 reverseListEl.style.backgroundColor = '#a8a8f0';
-reverseListEl.style.padding = '20px';
+reverseListEl.style.padding = '10px';
+reverseListEl.style.height = '100%';
 
 const highToLowNumbersArr = numbersArr.sort((a, b) => b - a);
 
@@ -66,28 +63,31 @@ highToLowNumbersArr.map((number) => {
 	reverseListEl.append(reverseListItemEl);
 	reverseListItemEl.style.backgroundColor = 'black';
 	reverseListItemEl.style.color = 'white';
+	reverseListItemEl.style.width = '50px';
 
 	if (number % 2 == 1) {
 		reverseListItemEl.style.backgroundColor = 'white';
 		reverseListItemEl.style.color = 'black';
 	}
-	number++;
-	console.log(number);
+
+	if (number === 8) {
+		reverseListItemEl.style.backgroundColor = 'transparent';
+	}
 });
 
 // ett-tio
 
 const arr = [
-	' ett ',
-	' två ',
-	' tre ',
-	' fyra ',
-	' fem ',
-	' sex ',
-	' sju ',
-	' åtta ',
-	' nio ',
-	' tio ',
+	'ett',
+	'två',
+	'tre',
+	'fyra',
+	'fem',
+	'sex',
+	'sju',
+	'åtta',
+	'nio',
+	'tio',
 ];
 
 const numbersInWordsEl = document.createElement('ul');
@@ -95,14 +95,25 @@ div.append(numbersInWordsEl);
 numbersInWordsEl.style.listStyleType = 'none';
 numbersInWordsEl.style.textAlign = 'end';
 numbersInWordsEl.style.backgroundColor = '#a8a8f0';
-numbersInWordsEl.style.padding = '20px';
+numbersInWordsEl.style.padding = '10px';
+numbersInWordsEl.style.height = '100%';
 
 for (const number of arr) {
 	const listItemEl = document.createElement('li');
 	listItemEl.innerText = number;
 	numbersInWordsEl.append(listItemEl);
+
 	listItemEl.style.backgroundColor = 'black';
 	listItemEl.style.color = 'white';
+	listItemEl.style.width = '50px';
 
-	console.log(number);
+	const index = arr.indexOf(number);
+	if (index % 2 == 1) {
+		listItemEl.style.backgroundColor = 'white';
+		listItemEl.style.color = 'black';
+	}
+
+	if (number === 'sex') {
+		listItemEl.style.backgroundColor = 'transparent';
+	}
 }
